40d39f6834ae50418561d28e69a1b2985045626f
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-04-01 13:44:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/javamail/Config.java
@@ -0,0 +1,6 @@
+package com.example.javamail;
+
+public class Config {
+    public static final String EMAIL ="GMAIL_ID";
+    public static final String PASSWORD ="GMAIL_PW";
+}
---
+++ app/src/main/java/com/example/javamail/Constants.java
@@ -0,0 +1,5 @@
+package com.example.javamail;
+
+public class Constants {
+    public static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 100;
+}
