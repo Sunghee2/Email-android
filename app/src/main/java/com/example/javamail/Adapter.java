2d543c8bc7fb9b9231076d01d14c2b6b2508f717
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-04-05 16:35:19 +0900

    Make list item

--- app/src/main/java/com/example/javamail/Adapter.java
+++ app/src/main/java/com/example/javamail/Adapter.java
@@ -43,7 +43,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.VHolder> {
 
         public VHolder(View itemView) {
             super(itemView);
-            textView = (TextView) itemView.findViewById(R.id.txtItem);
+            textView = (TextView) itemView.findViewById(R.id.textViewFrom);
         }
     }
 }
